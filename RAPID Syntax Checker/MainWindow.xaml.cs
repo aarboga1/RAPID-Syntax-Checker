d20bc9040bebdc9add174c82fbbed266367b786f
aarboga1 <35973505+aarboga1@users.noreply.github.com>
2018-06-06 19:09:55 -0400

    Added Open File Dialog

--- RAPID Syntax Checker/MainWindow.xaml.cs
+++ RAPID Syntax Checker/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 
 namespace RAPID_Syntax_Checker
 {
@@ -32,6 +34,29 @@ namespace RAPID_Syntax_Checker
         private void Exit_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            Open_File();
+
+        }
+        #endregion
+
+        #region [Commands]
+        private static void Open_File()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Title = "Open RGcode File";
+            openFileDialog.Filter = "MOD Files (*.mod)| *.mod";
+            
+            // Open window and check if file is valid
+            if (openFileDialog.ShowDialog() == true)
+            {
+                MessageBox.Show("It worked");
+            }
+
         }
         #endregion
 
